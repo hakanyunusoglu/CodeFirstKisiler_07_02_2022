a22c89b0054979d86d13f2396589c5d944d9278a
hakanyunusoglu <hakanyunusoglu93@gmail.com>
2022-02-07 14:17:07 +0300

    Add project files.

---
+++ CodeFirstKisiler/Controllers/AdresController.cs
@@ -0,0 +1,119 @@
+﻿using CodeFirstKisiler.Models;
+using CodeFirstKisiler.Models.Manager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodeFirstKisiler.Controllers
+{
+    public class AdresController : Controller
+    {
+        DatabaseContext db = new DatabaseContext();
+        public ActionResult YeniAdres()
+        {
+            List<SelectListItem> kisilerList = db.Kisiler.Select(x => new SelectListItem
+            {
+                Text = string.Concat(x.Ad + " " + x.Soyad),
+                Value = x.ID.ToString()
+            }).ToList();
+            TempData["Kisiler"] = kisilerList;
+            ViewBag.kisiler = kisilerList;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniAdres(Adres adres)
+        {
+            Kisiler kisi = db.Kisiler.FirstOrDefault(x => x.ID == adres.KisiId);
+            if (kisi != null)
+            {
+                adres.Kisiler = kisi;
+                db.Adresler.Add(adres);
+                
+                int sonuc = db.SaveChanges();
+                if (sonuc > 0)
+                {
+                    ViewBag.Result = "Adres başarıyla eklendi";
+                    ViewBag.Status = "success";
+
+                }
+                else
+                {
+                    ViewBag.Result = "Adres eklenememiştir";
+                    ViewBag.Status = "danger";
+                }
+                ViewBag.kisiler = TempData["Kisiler"];
+            }
+            return View();
+        }
+        public ActionResult Duzenle(int? adresid)
+        {
+            Adres adres = db.Adresler.FirstOrDefault(x=>x.ID == adresid);
+            Kisiler kisi = db.Kisiler.FirstOrDefault(x=>x.ID == adres.Kisiler.ID);
+            List<SelectListItem> kisilerList = db.Kisiler.Select(x => new SelectListItem
+            {
+                Text = string.Concat(x.Ad + " " + x.Soyad),
+                Value = x.ID.ToString(),
+                Selected = false
+            }).ToList();
+           
+            foreach(var item in kisilerList)
+            {
+
+                if (item.Value == kisi.ID.ToString())
+                {
+                    item.Selected = true;
+
+                }
+            }
+            TempData["Kisiler"] = kisilerList;
+            ViewBag.kisiler = kisilerList;
+            return View(adres);
+        }
+        [HttpPost]
+        public ActionResult Duzenle(Adres model,int? adresid)
+        {
+            Adres adres = db.Adresler.FirstOrDefault(x => x.ID == adresid);
+            if (adres != null)
+            {
+                adres.AdresTanim = model.AdresTanim;
+                adres.KisiId = model.KisiId;
+                adres.Kisiler = model.Kisiler;
+                int sonuc = db.SaveChanges();
+                if (sonuc > 0)
+                {
+                    ViewBag.Result = "Güncelleme işlemi başarıyla tamamlandı!";
+                    ViewBag.Status = "success";
+                }
+                else
+                {
+                    ViewBag.Result = "Güncelleme işlemi başarısız oldu!";
+                    ViewBag.Status = "danger";
+                }
+            }
+            return View();
+        }
+        public ActionResult Sil(int? adresid)
+        {
+            Adres adres = new Adres();
+            if (adresid != null)
+            {
+                adres = db.Adresler.FirstOrDefault(x => x.ID == adresid);
+            }
+            return View(adres);
+        }
+        [HttpPost, ActionName("Sil")]
+        public ActionResult SilOk(int? adresid)
+        {
+            Adres adres = new Adres();
+            if (adresid != null)
+            {
+                adres = db.Adresler.FirstOrDefault(x => x.ID == adresid);
+                db.Adresler.Remove(adres);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ CodeFirstKisiler/Controllers/KisiController.cs
@@ -0,0 +1,104 @@
+﻿using CodeFirstKisiler.Models;
+using CodeFirstKisiler.Models.Manager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodeFirstKisiler.Controllers
+{
+    public class KisiController : Controller
+    {
+        DatabaseContext db = new DatabaseContext();
+        // GET: Kisi
+        public ActionResult Yeni()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Yeni(Kisiler kisi)
+        {
+            db.Kisiler.Add(kisi);
+            int sonuc = db.SaveChanges();
+            if (sonuc > 0)
+            {
+                ViewBag.Result = "Kişi başarıyla eklendi";
+                ViewBag.Status = "success";
+
+            }
+            else
+            {
+                ViewBag.Result = "Kişi eklenememiştir";
+                ViewBag.Status = "danger";
+            }
+            return View();
+
+        }
+        public ActionResult Duzenle(int? kisiid)
+        {
+            Kisiler kisi = new Kisiler();
+            if (kisiid != null)
+            {
+                kisi = db.Kisiler.FirstOrDefault(x => x.ID == kisiid);
+            }
+            return View(kisi);
+        }
+        [HttpPost]
+        public ActionResult Duzenle(Kisiler model,int kisiid)
+        {
+            Kisiler kisi = db.Kisiler.FirstOrDefault(x=>x.ID == kisiid);
+            if(kisi != null)
+            {
+                kisi.Ad = model.Ad;
+                kisi.Adres = model.Adres;
+                kisi.Soyad = model.Soyad;
+                kisi.Yas = model.Yas;
+                int sonuc = db.SaveChanges();
+                if(sonuc > 0)
+                {
+                    ViewBag.Result = "Güncelleme işlemi başarıyla tamamlandı!";
+                    ViewBag.Status = "success";
+                }
+                else
+                {
+                    ViewBag.Result = "Güncelleme işlemi başarısız oldu!";
+                    ViewBag.Status = "danger";
+                }
+            }
+            return View();
+        }
+        public ActionResult Sil(int? kisiid)
+        {
+            Kisiler kisi = new Kisiler();
+            if(kisiid != null)
+            {
+                kisi = db.Kisiler.FirstOrDefault(x=>x.ID == kisiid);
+            }
+            return View(kisi);
+        }
+        [HttpPost,ActionName("Sil")]
+        public ActionResult SilOk(int? kisiid)
+        {
+            Kisiler kisi = new Kisiler();
+            List<Adres> adresList = db.Adresler.ToList();
+            if(kisiid != null)
+            {
+                kisi = db.Kisiler.FirstOrDefault(x=>x.ID == kisiid);
+                foreach(var item in adresList)
+                {
+                  if(item.Kisiler.ID == kisiid)
+                    {
+                        db.Adresler.Remove(item);
+                        db.SaveChanges();
+                    }
+                }
+                
+                db.Kisiler.Remove(kisi);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index","Home");
+        }
+
+    }
+}
